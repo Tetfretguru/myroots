eeb7b511598106d7b58a06ab5e5933a5aba5b138
Tetfretguru <josemartinghiena@gmail.com>
2022-07-04 20:47:33 -0300

    DP3: db stack WIP

--- db/stack.py
+++ db/stack.py
@@ -1,4 +1,10 @@
 import sqlite3
+import argparse
+import importlib.util
+from typing import List
+
+import pandas as pd
+
 from tables import (
     COUNTRIES_TABLE_SQL,
     PROVINCES_TABLE_SQL,
@@ -6,8 +12,20 @@ from tables import (
 )
 
 
+def create_insert_query(df: pd.DataFrame, table_name: str) -> str:
+    query = f"INSERT INTO {repr(table_name)} {tuple(df.columns)} VALUES"
+    for i,r in df.iterrows():
+        val = tuple(r.to_dict().values())
+        last = i == len(df) - 1
+        if last:
+            query += f"{str(val)};"
+        query += f"{str(val)},\n"
+    
+    return query
+
+    
 class MyRoots:
-    def __init__(self) -> None:
+    def __init__(self, queries: List[str]=[]) -> None:
         self.db_file = r"myroots.db"
 
         conn = None
@@ -15,6 +33,8 @@ class MyRoots:
         self.create_table(conn, "countries", COUNTRIES_TABLE_SQL)
         self.create_table(conn, "provinces", PROVINCES_TABLE_SQL)
         self.create_table(conn, "subdivisions", SUBDIVISIONS_TABLE_SQL)
+        if queries:
+            pass
         conn.close()
     
     @staticmethod
@@ -28,21 +48,46 @@ class MyRoots:
             print("Could not set connection")
             raise e
     
-    def create_table(self, conn, table_name, create_table_sql):
-        """ create a table from the create_table_sql statement
+    def execute_query(self, conn, query):
+        """ create a table from the query statement
         :param conn: Connection object
-        :param create_table_sql: a CREATE TABLE statement
+        :param query: a SQL query statement
         :return:
         """
         try:
-            print(f"Creating table {repr(table_name)}")
             c = conn.cursor()
-            c.execute(create_table_sql)
+            c.execute(query)
         except sqlite3.Error as e:
-            print(f"Error while creating table {repr(table_name)}")
+            print(f"Error while executing query: {repr(query)}")
+            raise e
+    
+    @staticmethod
+    def insert_into_table(conn: sqlite3.Connection, table_name: str, query: str) -> None:
+        """
+        Insert a set of data
+        :param conn: Connection object
+        :table_name: Target table
+        :data: Data source
+        """
+        try:
+            pass
+        except sqlite3.Error as e:
+            print(f"Error while inserting data in table {repr(table_name)}")
             raise e
 
 
 if __name__ == "__main__":
-    stack = MyRoots()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--country", type=str, help="Scrape and parse wikipedia wiki for such country", required=False)
+    args = parser.parse_args()
+    country: str = args.country
+    if country:
+        country_ = pd.read_csv(f"../buckets/source_locations_parsed/{country}/country.csv")
+        province = pd.read_csv(f"../buckets/source_locations_parsed/{country}/province.csv")
+        municipality = pd.read_csv(f"../buckets/source_locations_parsed/{country}/municipality.csv")
+        c_query = create_insert_query(country_, "countries")
+        p_query = create_insert_query(province, "provinces")
+        m_query = create_insert_query(municipality, "subdivisions")
+    else:
+        stack = MyRoots()
     print("Database stack has been created successfully")
